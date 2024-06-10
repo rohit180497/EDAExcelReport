140d3f68a160ac1bf2f597c294195f0ba38d19a2
rohit180497 <rohitkosamkar97@gmail.com>
2024-06-10 17:56:08 -0400

    readme file updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="EDAExcelReport",  
-    version="0.1.1",  
+    version="0.1.2",  
     author="Rohit Kosamkar",
     author_email="rohitkosamkar97@gmail.com",
     description="A package for generating EDA reports",
@@ -17,7 +17,14 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Visualization",
     ],
+    keywords="EDA Excel exploratory data analysis report pandas numpy openpyxl machine learning data science data analysis rohit kosamkar EDAExcelReport",
     python_requires='>=3.6',
     install_requires=[
         "pandas",  
