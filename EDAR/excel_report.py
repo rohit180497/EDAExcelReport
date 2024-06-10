3f4ec66e334c0a13643b6925166851a111449ba6
rohit180497 <rohitkosamkar97@gmail.com>
2024-06-10 16:00:37 -0400

    rename changes done

--- EDAR/excel_report.py
+++ EDAR/excel_report.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
-from ed_report.eda_format import EDA_Formatter 
+from EDAR.eda_format import EDA_Formatter 
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
 
